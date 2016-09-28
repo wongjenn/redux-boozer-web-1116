3c9e33e958f97bff41fc101938052aa33b273304
ipc103 <iancandysemail@gmail.com>
2016-09-28 14:22:54 -0400

    remove solution

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,1 @@
-export function fetchCocktails(){
-  // add your own cocktails here
-  const cocktails = [{id: 1, name: 'Gin and Tonic'}, {id: 2, name: 'Old Fashioned'}, {id: 3, name: 'Negroni'}, {id: 4, name: 'Roy Rogers'}];
-
-  return {
-    type: 'FETCH_COCKTAILS',
-    payload: cocktails
-  }
-}
+const defaultCocktails = [{id: 1, name: 'Gin and Tonic'}, {id: 2, name: 'Old Fashioned'}, {id: 3, name: 'Negroni'}, {id: 4, name: 'Roy Rogers'}];
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,2 @@
 import { combineReducers } from 'redux';
 import cocktailsReducer from './cocktails_reducer';
-
-const rootReducer =  combineReducers({
-  cocktails: cocktailsReducer
-});
-
-export default rootReducer;
