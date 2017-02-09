ebdc41c5203dd7965a64b59769d2684db0232458
wongjenn <wongjenn@yahoo.com>
2017-02-09 15:39:03 -0500

    generate show page with all the description, etc

--- src/components/cocktails_show.js
+++ src/components/cocktails_show.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 function CocktailsShow (props){
-  return(
+  return props.cocktail.name === undefined ? (<div></div>) :
+  (
     <div>
-    <h2></h2>
-      {props.cocktail.name}
+      <h2>{props.cocktail.name}</h2>
+      <p>{props.cocktail.description}</p>
+      <p>{props.cocktail.source}</p>
+      <p>Instructions: {props.cocktail.instructions}</p>
+      Proportions/Ingredients:
+      <ul>
+        {props.cocktail.proportions.map(proportion =>
+        <li key={proportion.id}>{proportion.amount} of {proportion.ingredient_name}</li>)}
+      </ul>
     </div>
   )
 }
